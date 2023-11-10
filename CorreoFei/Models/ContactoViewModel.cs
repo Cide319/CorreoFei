8ab3f1bb4f844c4daa81b1750da0acab94e491d4
Eduart Dircio <112608548+EduartDC@users.noreply.github.com>
2023-11-10 16:32:46 -0600

    Add project files.

---
+++ CorreoFei/Models/ContactoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CorreoFei.Models
+{
+    public class ContactoViewModel
+    {
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [Display(Name = "Nombre de Contacto")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [EmailAddress(ErrorMessage = "El campo {0} no es una dirección de correo electrónico válida")]
+        [Display(Name = "Correo Electrónico")]
+        public string Correo { get; set; }
+    }
+}
---
+++ CorreoFei/Services/Email/Email.cs
@@ -0,0 +1,63 @@
+﻿using CorreoFei.Services.ErrorLog;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+
+namespace CorreoFei.Services.Email
+{
+    public class Email : IEmail
+    {
+        private readonly IErrorLog _errorLog;
+        private readonly IConfiguration _configuration;
+
+        public Email(IErrorLog errorLog, IConfiguration configuration)
+        {
+            _errorLog = errorLog;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public Task<bool> EnviaCorreoAsync(string tema, string para, string cc, string bcc, string cuerpo, Attachment adjunto = null)
+        {
+            bool res = false;
+            try
+            {
+                MailMessage eMail = new();
+                if (para != null)
+                    eMail.To.Add(para);
+                if (cc != null)
+                    eMail.CC.Add(cc);
+                if (bcc != null)
+                    eMail.Bcc.Add(bcc);
+
+                eMail.From = new MailAddress(_configuration["Smtp:SmtpUser"]);
+                if (string.IsNullOrEmpty(tema))
+                    tema = "[sin asunto]";
+                eMail.Subject = tema;
+                eMail.Body = cuerpo;
+                if (adjunto != null)
+                    eMail.Attachments.Add(adjunto);
+                eMail.BodyEncoding = System.Text.Encoding.UTF8;
+                eMail.SubjectEncoding = System.Text.Encoding.UTF8;
+                eMail.HeadersEncoding = System.Text.Encoding.UTF8;
+                eMail.IsBodyHtml = true;
+
+                SmtpClient clienteSMTP = new(_configuration["Smtp:SmtpServer"]);
+                clienteSMTP.Port = Convert.ToInt16(_configuration["Smtp:SmtpPort"]);
+                clienteSMTP.EnableSsl = true;
+                clienteSMTP.DeliveryMethod = SmtpDeliveryMethod.Network;
+                clienteSMTP.UseDefaultCredentials = false;
+                clienteSMTP.Credentials = new System.Net.NetworkCredential(_configuration["Smtp:SmtpUser"], _configuration["Smtp:SmtpPwd"]);
+
+                _errorLog.ErrorLogAsync($"Enviando correo a: {para}");
+                clienteSMTP.SendAsync(eMail, null);
+                res = true;
+
+            }
+            catch (Exception ex)
+            {
+                _errorLog.ErrorLogAsync(ex.Message);
+            }
+            return Task.FromResult(res);
+        }
+    }
+}
